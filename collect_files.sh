b180c3d298c24439203c397d96f208eebb6b53ce
afzal <oldman11years@gmail.com>
2025-05-01 22:00:55 +0500

    Должно работать теперь

--- collect_files.sh
+++ collect_files.sh
@@ -5,10 +5,18 @@ if [[ -z "$1" || -z "$2" ]]; then
     exit 1
 fi
 
+if ! command -v java &> /dev/null; then
+    echo "Java is not installed. Installing OpenJDK..."
+    sudo apt-get update
+    sudo apt-get install -y default-jdk
+fi
+
 if [[ ! -f FileCollector.class ]]; then
-    javac FileCollector.java || { echo "Ошибка компиляции Java"; exit 1; }
+    javac FileCollector.java || { echo "Ошибк Java"; exit 1; }
 fi
 
 java FileCollector "$1" "$2"
+
+# Источник команд Bash: https://metanit.com/os/linux/12.8.php
 # Скрипт только запускает компилацию основного алгоритмa который реализован на java
 # Команды и их функции я нашел на https://metanit.com/os/linux/12.8.php
