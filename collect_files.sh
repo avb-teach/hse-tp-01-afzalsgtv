402cf174d4e357d77e7171149617f2446f778f83
afzal <oldman11years@gmail.com>
2025-05-02 12:44:38 +0500

    f5

--- collect_files.sh
+++ collect_files.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-
-if ! command -v java &> /dev/null; then
-    echo "Java не найдена. Устанавливаем..."
-    sudo apt update
-    sudo apt install default-jdk -y
-else
-    echo "Java уже установлена"
+if [[ -z "$1" || -z "$2" ]]; then
+    echo "Usage: ./collect_files.sh /input/dir /output/dir"
+    exit 1
 fi
+
+
+java FileCollector "$1" "$2"
 # Скрипт только запускает компилацию основного алгоритмa который реализован на java
 # Команды и их функции я нашел на https://metanit.com/os/linux/12.8.php
