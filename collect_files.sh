dbf45db6844353b4143763e7a89b53ba80eb28f1
afzal <oldman11years@gmail.com>
2025-05-02 12:53:48 +0500

    e

--- collect_files.sh
+++ collect_files.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
+# Проверка наличия двух аргументов
+if [[ -z "$1"  -z "$2" ]]; then
+  echo "Usage: ./collect_files.sh /input/dir /output/dir"
+  exit 1
+fi
 
+if ! command -v java &> /dev/null; then
+  echo "Java is not installed. Installing..."
+  sudo apt update && sudo apt install -y default-jdk
+fi
 
 
 java FileCollector "$1" "$2"
